c8098169d4ef2fdecb9e3ca53368445f46dc84f4
Kainã Fontoura Almeida <kainafa@hotmail.com>
2020-10-01 23:26:02 -0300

    KA - Testes iniciais

---
+++ test/CursoOnline.Domain.Test/Cursos/CursoTest.cs
@@ -0,0 +1,117 @@
+﻿using CursoOnline.Domain.Test._Util;
+using ExpectedObjects;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CursoOnline.Domain.Test.Cursos
+{
+    public class CursoTest : IDisposable
+    {
+        private readonly ITestOutputHelper _outPut;
+        private readonly string _nome;
+        private readonly double _cargaHoraria;
+        private readonly PublicoAlvo _publicoAlvo;
+        private readonly double _valor;
+
+        //Setup
+        public CursoTest(ITestOutputHelper outPut)
+        {
+            _outPut = outPut;
+            _outPut.WriteLine("teste sendo executado");
+
+            _nome = "Informática básica";
+            _cargaHoraria = 80;
+            _publicoAlvo = PublicoAlvo.Estudante;
+            _valor = 950;
+
+        }
+
+        [Fact(DisplayName = "DeveCriarCurso")]
+        public void DeveCriarCurso()
+        {
+            var cursoEsperado = new
+            {
+                Nome = _nome,
+                CargaHoraria = _cargaHoraria,
+                PublicoAlvo = _publicoAlvo,
+                Valor = _valor
+            };
+
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            new Curso(nomeInvalido, _cargaHoraria, _publicoAlvo, _valor))
+                .ComMensagem("Nome inválido");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmaCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            new Curso(_nome, cargaHorariaInvalida, _publicoAlvo, _valor))
+                .ComMensagem("Carga horária inválida");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmValorMenorQue1(double valorInvalido)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            new Curso(_nome, _cargaHoraria, _publicoAlvo, valorInvalido))
+                .ComMensagem("Valor inválido");
+        }
+
+        //Cleanup
+        public void Dispose()
+        {
+            _outPut.WriteLine("Cleanup");
+        }
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
+    }
+
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome inválido");
+
+            if (cargaHoraria < 1)
+                throw new ArgumentException("Carga horária inválida");
+
+            if (valor < 1)
+                throw new ArgumentException("Valor inválido");
+
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+    }
+}
---
+++ test/CursoOnline.Domain.Test/_Util/AssertExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace CursoOnline.Domain.Test._Util
+{
+    public static class AssertExtension
+    {
+        public static void ComMensagem(this ArgumentException exception, string message)
+        {
+            if (exception.Message == message)
+                Assert.True(true);
+            else
+                Assert.False(true, $"Esperava a mensagem {message}");
+        }
+    }
+}
