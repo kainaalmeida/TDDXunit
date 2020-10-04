34e1a9aef4c151b08598e42cb71b106d11f4ddf2
Kainã Almeida <kaina.almeida@xpi.com.br>
2020-10-04 20:52:12 -0300

    Utilização de MOQ para simular comportamentos do service

---
+++ test/CursoOnline.Domain.Test/Cursos/ArmazenadorDeCursoTest.cs
@@ -0,0 +1,70 @@
+﻿using Bogus;
+using CursoOnline.Domain.Cursos;
+using Moq;
+using Xunit;
+
+namespace CursoOnline.Domain.Test.Cursos
+{
+    public class ArmazenadorDeCursoTest
+    {
+        private readonly CursoDto _cursoDto;
+        private readonly Mock<ICursoRepositorio> _cursoRepositorioMock;
+        private readonly ArmazenadorDeCurso _armazenadorDeCurso;
+
+        public ArmazenadorDeCursoTest()
+        {
+
+            var faker = new Faker();
+            _cursoDto = new CursoDto
+            {
+                Nome = faker.Random.Words(),
+                Descricao = faker.Lorem.Paragraph(),
+                CargaHoraria = faker.Random.Double(500, 1000),
+                PublicoAlvoId = 1,
+                Valor = faker.Random.Double(100, 1000)
+            };
+
+            _cursoRepositorioMock = new Mock<ICursoRepositorio>();
+            _armazenadorDeCurso = new ArmazenadorDeCurso(_cursoRepositorioMock.Object);
+        }
+
+        [Fact]
+        public void DeveAdicionarCurso()
+        {
+            _armazenadorDeCurso.Armazenar(_cursoDto);
+
+            _cursoRepositorioMock.Verify(r => r.Adicionar(It.Is<Curso>(c => c.Nome == _cursoDto.Nome)));
+        }
+    }
+
+    public interface ICursoRepositorio
+    {
+        void Adicionar(Curso curso);
+    }
+
+    public class ArmazenadorDeCurso
+    {
+        private ICursoRepositorio _cursoRepositorio;
+
+        public ArmazenadorDeCurso(ICursoRepositorio cursoRepositorio)
+        {
+            _cursoRepositorio = cursoRepositorio;
+        }
+
+        public void Armazenar(CursoDto cursoDto)
+        {
+            var curso = new Curso(cursoDto.Nome, cursoDto.Descricao, cursoDto.CargaHoraria, PublicoAlvo.Estudante, cursoDto.Valor);
+
+            _cursoRepositorio.Adicionar(curso);
+        }
+    }
+
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double CargaHoraria { get; set; }
+        public int PublicoAlvoId { get; set; }
+        public double Valor { get; set; }
+    }
+}
